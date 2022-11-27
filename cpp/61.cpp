fa2d69fca85f924d380320095412d53680d2da26
renpeng <renpeng@p1.com>
2022-11-27 13:05:46 +0800

    add

--- cpp/61.cpp
+++ cpp/61.cpp
@@ -11,16 +11,29 @@ struct ListNode {
 class Solution {
 public:
   ListNode *rotateRight(ListNode *head, int k) {
-    int cnt = 0;
-    ListNode *p = head;
-    while (p != nullptr) {
-      p = p->next;
-      ++cnt;
+    if (head == nullptr || k == 0) {
+      return head;
     }
 
-    if (k > cnt) {
-      k = k - cnt;
+    ListNode *end = head;
+    int cnt = 0;
+
+    while (end->next != nullptr) {
+      end = end->next;
+      ++cnt;
     }
+    ++cnt;
+
+    if (cnt == 1) {
+      return head;
+    }
+
+    head = reverse(head, end);
+
+    if (k > cnt) {
+      k = k % cnt;
+    }
+    k = (cnt - k) % cnt;
 
     ListNode *fast = head;
     ListNode *slow = head;
@@ -36,43 +49,27 @@ public:
     }
 
     ListNode *t = slow->next;
-
     ListNode *node1 = reverse(head, slow);
 
-    // log start
-    ListNode *temp = node1;
-    std::cout << "node1" << std::endl;
-    while (temp != nullptr) {
-      std::cout << temp->val << std::endl;
-      temp = temp->next;
-    }
-    // log end
-
     ListNode *node2 = reverse(t, fast);
+    ListNode *newHead = node1;
 
-    // log start
-    temp = node2;
-    std::cout << "node2" << std::endl;
-    while (temp != nullptr) {
-      std::cout << temp->val << std::endl;
-      temp = temp->next;
-    }
-    // log end
-
-    ListNode *newHead = node2;
-
-    while (node2->next != nullptr) {
-      node2 = node2->next;
+    while (node1->next != nullptr) {
+      node1 = node1->next;
     }
 
-    node2->next = node1;
-
+    node1->next = node2;
     return newHead;
   }
 
   ListNode *reverse(ListNode *beginNode, ListNode *endNode) {
 
+    if (beginNode == nullptr || endNode == nullptr) {
+      return nullptr;
+    }
+
     if (beginNode == endNode) {
+      beginNode->next = nullptr;
       return beginNode;
     }
 
@@ -86,9 +83,7 @@ public:
       next = next->next;
       temp->next = currnet;
       currnet = temp;
-      std::cout << "555555" << std::endl;
     }
-    // temp->next = currnet;
     next->next = currnet;
     return next;
   }
@@ -106,6 +101,15 @@ int main() {
   node3->next = node4;
   node4->next = node5;
 
+  // ListNode *node1 = new ListNode(1);
+  // ListNode *node2 = new ListNode(2);
+  // ListNode *node3 = new ListNode(3);
+
+  // node1->next = node2;
+  // node2->next = node3;
+
+  // ListNode *node1 = new ListNode(1);
+
   ListNode *originListNode = node1;
   std::cout << "origin list node" << std::endl;
   while (originListNode != nullptr) {
@@ -115,7 +119,7 @@ int main() {
 
   Solution *solution = new Solution();
 
-  ListNode *node = solution->rotateRight(node1, 2);
+  ListNode *node = solution->rotateRight(node1, 10);
 
   std::cout << "merge result" << std::endl;
   while (node != nullptr) {
