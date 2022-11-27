83dbec2c483e9e07a0c9895100e78856a4caa394
renpeng <renpeng@p1.com>
2022-11-27 11:07:52 +0800

    add

--- cpp/234.cpp
+++ cpp/234.cpp
@@ -12,7 +12,7 @@ struct ListNode {
 
 class Solution {
 public:
-  bool isPalindrome(ListNode *head) {
+  bool isPalindrome2(ListNode *head) {
     std::vector<int> vec;
     while (head != nullptr) {
       vec.push_back(head->val);
@@ -33,7 +33,79 @@ public:
     return true;
   }
 
-  bool isPalindrome2(ListNode *head) {}
+  bool isPalindrome3(ListNode *head) {
+    int cnt = 0;
+    ListNode *p = head;
+
+    while (p != nullptr) {
+      ++cnt;
+      p = p->next;
+    }
+
+    bool isOdd = (cnt % 2 == 1);
+
+    std::stack<int> mStack;
+    int cntNew = 0;
+
+    while (cntNew < cnt / 2) {
+      mStack.push(head->val);
+      ++cntNew;
+      head = head->next;
+    }
+
+    if (isOdd) {
+      head = head->next;
+    }
+
+    while (head != nullptr) {
+      if (mStack.top() != head->val) {
+        return false;
+      }
+
+      mStack.pop();
+      head = head->next;
+    }
+
+    return true;
+  }
+
+  //  使用vector 代替stack 内内存占用并没有提升反而略微下降
+  bool isPalindrome(ListNode *head) {
+    int cnt = 0;
+    ListNode *p = head;
+
+    while (p != nullptr) {
+      ++cnt;
+      p = p->next;
+    }
+
+    bool isOdd = (cnt % 2 == 1);
+
+    std::vector<int> vec;
+    vec.reserve(cnt / 2);
+    int cntNew = 0;
+
+    while (cntNew < cnt / 2) {
+      vec.push_back(head->val);
+      ++cntNew;
+      head = head->next;
+    }
+
+    if (isOdd) {
+      head = head->next;
+    }
+
+    while (head != nullptr) {
+      if (vec.back() != head->val) {
+        return false;
+      }
+
+      vec.pop_back();
+      head = head->next;
+    }
+
+    return true;
+  }
 };
 
 int main() {
